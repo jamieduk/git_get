0e9288dae53a3b4e5db77ea83a0db0a4b8c9969f
Jay <jamied_uk@hotmail.com>
2020-10-03 19:42:10 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,19 +8,12 @@
 #
 #
 # https://jnet.forumotion.com/t1714-git-get-for-github-it-downloads-github-repos-auto-with-bash#2656
-
 server="server" # Server Name
-
 username="jusername" # administrator
-
 password="password" # password
-
 share="NETWORK" # Share Name
-
 filename="GITHUB.txt" # File Name
-
 path="/home/$USER/Documents/Scripts/BASH/Get_Git/" # Path to copy filename to.
-
 installpath="/home/$USER/Documents/Scripts/"
 echo "Welcome To Git Get By J~Net 2020 jnet.sytes.net"
 echo "Copying File Over Ready For DL"
@@ -28,17 +21,18 @@ echo ""
 # smbclient '//windowsserver/c$' -c 'lcd /tmp; cd Directory; get file.txt' -U administrator%password
 smbclient "//$server/$share" -c "lcd $path; cd Directory; get $filename" -U $username%$password
 #
-cd $installpath
+#cd $installpath
 while IFS="" read -r p || [ -n "$p" ]
 do
+
   printf '%s\n' "$p"
-  
+ 
 done < $filename
 #
 for i in $(cat $filename)
 do
-     git clone $i # sudo
-done
 
-# Remove temp file
+    $1 # sudo git clone $i 
+done
+#
 # rm $filename
