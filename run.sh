e146f94c1cbfc9bcda23fd5b4dbf4e4b946bdabd
Jay <jamied_uk@hotmail.com>
2020-10-03 20:26:41 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,87 +1,44 @@
 #!/bin/bash
-
 # (c) J~Net 2020
-
 #
-
 # jnet.sytes.net
-
 #
-
 # Example to run make executable then type:
-
 # ./run.sh 
-
 #
-
 #
-
 # https://jnet.forumotion.com/t1714-git-get-for-github-it-downloads-github-repos-auto-with-bash#2656
-
 server="server" # Server Name
-
 username="username" # administrator
-
 password="password" # password
-
 share="NETWORK" # Share Name
-
 filename="GITHUB.txt" # File Name
-
 path="/home/$USER/Documents/Scripts/BASH/Get_Git/" # Path to copy filename to.
-
 installpath="/home/$USER/Documents/Scripts/"
-
 fallback="https://raw.githubusercontent.com/jamieduk/git_get/main/GITHUB.txt"
-
 #
-
 echo "Welcome To Git Get By J~Net 2020 jnet.sytes.net"
-
 echo "Copying File Over Ready For DL"
-
 echo ""
-
 # smbclient '//windowsserver/c$' -c 'lcd /tmp; cd Directory; get file.txt' -U administrator%password
-
 smbclient "//$server/$share" -c "lcd $path; cd Directory; get $filename" -U $username%$password
-
 #
-
 #cd $installpath
-
 if [ ! -e "$filename" ]; then
-
     echo "File does not exist"
-
     echo "FallBack Activated!"
-
     wget $fallback
-
 else 
-
     
-
 fi 
 
-
-
 while IFS="" read -r p || [ -n "$p" ]
-
 do
-
   git clone $p
-
   
-
 done < $filename
-
 #
-
 echo "All Commands Have Been Ran!"
-
 #
-
 #
-
 # rm $filename
