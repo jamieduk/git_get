b644bfc2a0144f3a1a46627c8d91b1cab6d06947
Jay <jamied_uk@hotmail.com>
2022-02-03 13:40:39 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,28 +13,17 @@ username="username" # administrator
 password="password" # password
 share="NETWORK" # Share Name
 filename="GITHUB.txt" # File Name
-path="/home/$USER/Documents/Scripts/BASH/Get_Git/" # Path to copy filename to.
+path="/home/$USER/Documents/Scripts/Get_Git/" # Path to copy filename to.
 installpath="/home/$USER/Documents/Scripts/"
 fallback="https://raw.githubusercontent.com/jamieduk/git_get/main/GITHUB.txt"
 #
 echo "Welcome To Git Get By J~Net 2020 jnet.sytes.net"
 echo "Copying File Over Ready For DL"
 echo ""
-# smbclient '//windowsserver/c$' -c 'lcd /tmp; cd Directory; get file.txt' -U administrator%password
-smbclient "//$server/$share" -c "lcd $path; cd Directory; get $filename" -U $username%$password
-#
-#cd $installpath
-if [ ! -e "$filename" ]; then
-    echo "File does not exist"
-    echo "FallBack Mode Activated!"
-    wget $fallback
-else 
-    echo "File Found!"
-fi 
 
 while IFS="" read -r p || [ -n "$p" ]
 do
-  $p # git clone   
+  git clone $p # git clone   
 done < $filename
 #
 echo "All Commands Have Been Ran!"
